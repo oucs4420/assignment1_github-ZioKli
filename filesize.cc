8e907cfc50e0c70dcacfdc1c2eb17221fff281bc
Zion Klinger <zion.klinger1@gmail.com>
2022-01-13 23:13:40 -0500

    started outlining solution

--- filesize.cc
+++ filesize.cc
@@ -1,18 +1,31 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 // output format to use IDENTICALLY:
-//BSH:Saru> ./filesize input.3lines input.1line 
-//program: ./filesize
+// BSH:Saru> ./filesize input.3lines input.1line
+// program: ./filesize
 // input.3lines: 3
 // input.1line: 1
 
-int main( int argc, char* argv[] )
-{
-    // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+int count_lines(string file_name) {
+    int count = 0;
+    ifstream ifs;
+    string _dump;
+    ifs.open(file_name);
+    if (ifs.is_open()) {
+        while (getline(ifs, _dump)) {
+            count++;
+        }
+    }
+    return count;
+}
 
+int main(int argc, char *argv[]) {
+    // just to get you started, this is how to refer to the arguments that were
+    // passed
+    for (int arg = 1; arg < argc; ++arg) {
+        cout << argv[arg] << ": " << count_lines(argv[arg]) << endl;
+    }
     exit(0); // this means that the program executed correctly!
 }
